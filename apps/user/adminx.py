7990f430696d770cabaabf6b695c8744106a4a1f
disen-xaqf <610039018@qq.com>
2019-06-25 18:00:52 +0800

    add user profile

--- apps/user/adminx.py
+++ apps/user/adminx.py
@@ -1,14 +1,44 @@
 import xadmin as admin
+from xadmin.views import CommAdminView, BaseAdminView
 
-
-from user.models import LoginUser
-from user.forms import LoginUserForm
+from user.models import LoginUser, UserProfile
 # Register your models here.
 
+class GlobalSettings(object):
+    # 整体配置
+    site_title = 'xxx后台管理系统'
+    site_footer = '西安千锋 Python1901'
+    menu_style = 'accordion'  # 菜单折叠
+    # 搜索模型
+    global_search_models = [LoginUser]
+
+    # 模型的图标(参考bootstrap图标插件)
+    global_models_icon = {
+        LoginUser: "glyphicon glyphicon-user"
+    }  # 设置models的全局图标
+
+    # 设置app模块的标题
+    apps_label_title = {
+        'user': '客户管理'
+    }
+
+    # 设置app模块的图标
+    apps_icons = {
+        'user': 'glyphicon glyphicon-user'
+    }
+
+
 class LoginUserAdmin():
-    form = LoginUserForm
     list_display = ['login_name', 'create_time', 'update_time']
     list_per_page = 20
 
 
+class UserProfileAdmin():
+    list_display = ['user', 'nick_name', 'real_name', 'card', 'phone']
+    search_fields = ['nick_name', 'phone']
+    list_per_page = 20
+
+
+admin.site.register(CommAdminView, GlobalSettings)
 admin.site.register(LoginUser, LoginUserAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
--- apps/user/models.py
+++ apps/user/models.py
@@ -2,7 +2,6 @@ from django.contrib.auth.hashers import make_password
 from django.db import models
 
 
-
 # Create your models here.
 class LoginUser(models.Model):
     login_name = models.CharField(max_length=20,
@@ -13,6 +12,7 @@ class LoginUser(models.Model):
                                        auto_now_add=True)
     update_time = models.DateTimeField(verbose_name='更新时间',
                                        auto_now=True)
+    is_activated = models.BooleanField(verbose_name='是否激活', default=False)
     note = models.CharField(max_length=50,
                             blank=True,
                             null=True)
@@ -27,9 +27,35 @@ class LoginUser(models.Model):
             self.login_auth_str = make_password(self.login_auth_str)
         super().save()
 
-
     class Meta:
         db_table = 'app_user_2'
         verbose_name = '用户表'
         verbose_name_plural = verbose_name
 
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(LoginUser,
+                                verbose_name='用户ID',
+                                on_delete=models.CASCADE)
+
+    nick_name = models.CharField(max_length=20, verbose_name='昵称', null=True, blank=True)
+    phone = models.CharField(max_length=15, verbose_name='手机号', null=True, blank=True)
+    photo = models.ImageField(verbose_name='用户头像',
+                              upload_to='users/photo', null=True, blank=True)
+
+    real_name = models.CharField(max_length=20, verbose_name='真实姓名', null=True, blank=True)
+    image_forward = models.ImageField(verbose_name='身份正面照片', upload_to='users/photo',null=True, blank=True)
+    image_back = models.ImageField(verbose_name='身份反面照片', upload_to='users/photo',null=True, blank=True)
+    card = models.CharField(max_length=20, verbose_name='身份证号码', null=True, blank=True)
+
+    def delete(self, using=None, keep_parents=False):
+        # 删除文件
+        super().delete()
+
+    def __str__(self):
+        return self.user.login_name
+
+    class Meta:
+        db_table = 'app_user_profile'
+        verbose_name = '用户详情'
+        verbose_name_plural = verbose_name
